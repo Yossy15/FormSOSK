36731eb5f37283754b53d731ce2efd54f2d25ef7
Yossy15 <gameimp72@gmail.com>
2025-07-20 16:43:16 +0700

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const User = mongoose.model("User", userSchema);
 
 app.use(bodyParser.json());
 
-app.set('view engine', 'ejs'); 
+app.set('view engine', 'html'); 
 app.use(express.static('views'));
 
 app.get('/', (req, res) => { 
